de1fdaccebc4c3d43bd68a10b6b93df1a65943df
Pawan Kumar <pk.soft29@gmail.com>
2020-09-11 09:51:14 +0530

    Added : Singlet Combination stored into dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
+h=0;
+t=0;
+declare -A Singlet
+read -p "Enter the times of flip" num;
+for ((counter=1; counter < $num; counter++))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+		h=$((h+1))
+		Singlet[1]=$h
+	else
+		t=$((t+1))
+		Singlet[2]=$t
+	fi
+done
+	SingHeadProb=$((${Singlet[1]} *100 / $num ))
+	SingTailProb=$((${Singlet[2]} *100 / $num ))
+echo "----------------------------------------------------------------------------------------"
+if [ $SingHeadProb -ge $SingTailProb ]
 then
-	echo "Head"
+	echo "Head = $SingHeadProb %"
 else
-	echo "Tail"
+	echo "Tail = $SingTailProb %"
 fi
+
+echo "Head: ${Singlet[1]} Tail: ${Singlet[2]}"
